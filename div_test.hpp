13c663657654836e060336337c46d792ae10b73e
iwu021 <iwu021@ucr.edu>
2021-04-20 00:35:05 -0700

    Created div and sub with tests

---
+++ div_test.hpp
@@ -0,0 +1,35 @@
+#ifndef _DIV_TEST_HPP_
+#define _DIV_TEST_HPP_
+
+#include "gtest/gtest.h"
+#include "div.hpp"
+#include "op.hpp"
+
+TEST(DivTest, DivPos)
+{
+        Op *first = new Op(10);
+        Op *second = new Op(5);
+        Div * test = new Div(first,second);
+        EXPECT_EQ(test->evaluate(),2);
+        EXPECT_EQ(test->stringify(), "(10.000000/5.000000)");
+}
+
+TEST(DivTest, DivNeg)
+{
+        Op *first = new Op(-2);
+        Op *second = new Op(2);
+        Div *test = new Div(first,second);
+        EXPECT_EQ(test->evaluate(),-1);
+        EXPECT_EQ(test->stringify(), "(-2.000000/2.000000)");
+}
+
+TEST(DivTest, DivZero)
+{
+        Op *first = new Op(0);
+        Op *second = new Op(2);
+        Div *test = new Div(first,second);
+        EXPECT_EQ(test->evaluate(),0);
+        EXPECT_EQ(test->stringify(), "(0.000000/2.000000)");
+}
+
+
---
+++ sub.hpp
@@ -0,0 +1,28 @@
+#ifndef _SUB_HPP_
+#define _SUB_HPP_
+#include "base.hpp"
+
+class Sub: public Base
+{
+        private:
+                Base *left;
+                Base *right;
+        public:
+                Sub(Base* left, Base *right)
+                {
+                        this->left = left;
+                        this->right = right;
+                }
+
+                double evaluate()
+                {
+                        return left->evaluate()-right->evaluate();
+                }
+
+                std::string stringify()
+                {
+                        return "(" + left->stringify() + "-" + right->stringify()+ ")";
+                }
+};
+#endif
+
